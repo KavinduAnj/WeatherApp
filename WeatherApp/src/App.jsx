252e27124dbf14cd9cb5bf5543dd86b755290e86
Kavindu <wkanjana1590@gmail.com>
2025-09-29 22:59:56 +0530

    forecast

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
     const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code));
 
     setCurrentWeather({temperature, description, weatherIcon });
+    console.log(data);
    } catch(error){
     console.log(error); 
    }
