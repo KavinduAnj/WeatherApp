814d19eabfd42bab531a5a0f26ac035d1414e60b
Kavindu <wkanjana1590@gmail.com>
2025-10-25 23:43:56 +0530

    h.w

--- WeatherApp/src/components/HourlyWeatherItem.jsx
+++ WeatherApp/src/components/HourlyWeatherItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { weatherCodes } from '../constants'
 
-const HourlyWeatherItem = () => {
+const HourlyWeatherItem = ({hourlyWeather}) => {
+  const temparature = Math.floor(hourlyWeather.temp_c);
+  const time = hourlyWeather.time;
+  const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(hourlyWeather.current.condition.code));
   return (
     <li className="weather-item">
-              <p className="time">00:00</p>
-              <img src="icons/clouds.svg" alt="weather-icon" className='weather-icon' />
-              <p className="temp">20<span>°C</span></p>
+              <p className="time">{time}</p>
+              <img src={`icons/${weatherIcon}.svg`} alt="weather-icon" className='weather-icon' />
+              <p className="temperature">{temparature}°</p>
             </li>
   )
 }
