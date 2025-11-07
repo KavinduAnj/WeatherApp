f0c9b09a94d026a2f04ada6c07309d029d439a70
Kavindu <wkanjana1590@gmail.com>
2025-11-07 23:32:33 +0530

    forcast

--- WeatherApp/src/components/HourlyWeatherItem.jsx
+++ WeatherApp/src/components/HourlyWeatherItem.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { weatherCodes } from '../constants'
 
-const HourlyWeatherItem = ({hourlyWeather}) => {
-  const temparature = Math.floor(hourlyWeather.temp_c);
+const HourlyWeatherItem = ({ hourlyWeather }) => {
+  if (!hourlyWeather || !hourlyWeather.condition) {
+    return null;
+  }
+
+  const temperature = Math.floor(hourlyWeather.temp_c);
   const time = hourlyWeather.time;
-  const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(hourlyWeather.current.condition.code));
+  const weatherIcon = Object.keys(weatherCodes).find(
+    icon => weatherCodes[icon].includes(hourlyWeather.condition.code)
+  );
+
   return (
     <li className="weather-item">
-              <p className="time">{time}</p>
-              <img src={`icons/${weatherIcon}.svg`} alt="weather-icon" className='weather-icon' />
-              <p className="temperature">{temparature}°</p>
-            </li>
+      <p className="time">{time}</p>
+      {weatherIcon && (
+        <img
+          src={`icons/${weatherIcon}.svg`}
+          alt="weather-icon"
+          className="weather-icon"
+        />
+      )}
+      <p className="temperature">{temperature}°</p>
+    </li>
   )
 }
 
-export default HourlyWeatherItem
+export default HourlyWeatherItem;
